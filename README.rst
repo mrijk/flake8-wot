fb3e9914dc2102e0ed4823b8e90d68a9c7d51167
Maurits <maurits.rijk@gmail.com>
2022-01-17 10:59:18 +0100

    Minor cleanup

--- README.rst
+++ README.rst
@@ -1,4 +1,10 @@
 # flake8-wot
 Flake8 plugin
 
-This plugin warns if you are still using List, Dict or Tuple in your code instead of list, dict, tuple.
+This plugin warns if you are still importing List, Dict or Tuple (and others) from typing instead of using list, dict,
+tuple.
+
+See https://www.python.org/dev/peps/pep-0585/ for more information.
+
+WOT is an abbreviation of 'Warn about Old school Types'
+
--- flake8_warn_old_school_types.py
+++ flake8_warn_old_school_types.py
@@ -1,10 +1,10 @@
 import importlib.metadata
-from _ast import ImportFrom, AnnAssign
+from _ast import ImportFrom, AnnAssign, Assign, expr
 from ast import NodeVisitor, AST
 from typing import Generator, Any
 
 
-OLD_SCHOOL_TYPES = ["List", "Tuple", "Dict"]
+OLD_SCHOOL_TYPES = ["Dict", "List", "Set", "Tuple"]
 
 
 def _old_school(type_name: str) -> bool:
@@ -14,19 +14,30 @@ def _old_school(type_name: str) -> bool:
 class Visitor(NodeVisitor):
 
     def __init__(self):
-        self.problems_001: list[tuple[int, int, str]] = []
-        self.problems_002: list[tuple[int, int, str]] = []
+        self.problems: dict[str, list[tuple[int, int, str]]] = {
+            "WOT001": [],
+            "WOT002": [],
+        }
 
-    def visit_ImportFrom(self, node: ImportFrom) -> None:
-        for name in node.names:
-            if _old_school(name.name):
-                self.problems_001.append((node.lineno, node.col_offset, name.name))
-        self.generic_visit(node)
+    def _report_problem(self, key: str, node: AST, type_name: str):
+        self.problems[key].append((node.lineno, node.col_offset, type_name))
 
     def visit_AnnAssign(self, node: AnnAssign) -> Any:
         type_name = node.annotation.value.attr
         if _old_school(type_name):
-            self.problems_002.append((node.lineno, node.col_offset, type_name))
+            self._report_problem("WOT002", node, type_name)
+
+    def visit_ImportFrom(self, node: ImportFrom) -> None:
+        for name in node.names:
+            type_name = name.name
+            if _old_school(type_name):
+                self._report_problem("WOT001", node, type_name)
+        self.generic_visit(node)
+
+    def visit_Assign(self, node: Assign) -> Any:
+        type_name = node.value.attr
+        if _old_school(type_name):
+            self._report_problem("WOT002", node, type_name)
 
 
 class Plugin:
@@ -37,9 +48,9 @@ class Plugin:
         self._tree = tree
 
     def _report(self, visitor: Visitor) -> Generator[tuple[int, int, str, type[Any]], None, None]:
-        for line, col, old_type in visitor.problems_001:
+        for line, col, old_type in visitor.problems["WOT001"]:
             yield line, col, f"WOT001 don't import type {old_type}", type(self)
-        for line, col, old_type in visitor.problems_002:
+        for line, col, old_type in visitor.problems["WOT002"]:
             yield line, col, f"WOT002 don't use type typing.{old_type}", type(self)
 
     def run(self) -> Generator[tuple[int, int, str, type[Any]], None, None]:
